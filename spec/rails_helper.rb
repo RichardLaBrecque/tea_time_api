7107dd4ef23749a2766348d172c76b635ed5a6e3
Richard LaBrecque <RichardlaBreque@gmail.com>
2022-06-01 12:50:02 -0400

    feat: setup migrations and relationships

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -5,6 +5,8 @@ require_relative '../config/environment'
 # Prevent database truncation if the environment is production
 abort("The Rails environment is running in production mode!") if Rails.env.production?
 require 'rspec/rails'
+require 'simplecov'
+SimpleCov.start
 # Add additional requires below this line. Rails is not loaded until this point!
 
 # Requires supporting ruby files with custom matchers and macros, etc, in
@@ -61,4 +63,21 @@ RSpec.configure do |config|
   config.filter_rails_from_backtrace!
   # arbitrary gems may also be filtered via:
   # config.filter_gems_from_backtrace("gem name")
+end
+Shoulda::Matchers.configure do |config|
+  config.integrate do |with|
+    with.test_framework :rspec
+    with.library :rails
+  end
+  VCR.configure do |config|
+  config.cassette_library_dir = "spec/fixtures/vcr_cassettes"
+  config.hook_into :webmock
+  # filters sensitive data for us. Each API key will need it's own line
+  config.filter_sensitive_data('app_key') { ENV['app_key'] }
+  # allows us to use the names of our tests to name cassettes rather than having to manually wrap each test in a block
+  # add :vcr do to our test block after the first one
+  config.configure_rspec_metadata!
+  # re-record default configuration block (instead of in specific test)
+  config.default_cassette_options = { re_record_interval: 2.days }
+end
 end
